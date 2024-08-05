cd9ef39e944b2376db0efc4a96ef83ebcc35eef7
MZIO <mziodev@gmail.com>
2024-08-05 12:15:49 +0200

    [Feature] Add group and moving rows test

--- Monee/ContentView.swift
+++ Monee/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section("Groups") {
+                    ForEach(1...3, id: \.self) { group in
+                        DisclosureGroup("Group \(group)") {
+                            ForEach(1...5, id: \.self) {
+                                Text($0.description)
+                            }
+                        }
+                    }
+                    .onMove(perform: { indices, newOffset in
+                        //
+                    })
+                }
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Test")
+            .toolbar {
+                EditButton()
+            }
         }
-        .padding()
     }
 }
 
