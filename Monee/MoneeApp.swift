9b43aa6ab71813bb89a6a9b87491ca501308bc44
MZIO <mziodev@gmail.com>
2024-08-19 17:01:46 +0200

    Initial Commit

---
+++ Monee/MoneeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneeApp.swift
+//  Monee
+//
+//  Created by MZiO on 19/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MoneeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
